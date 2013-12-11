c4aa731e25ddb77870b391a307b38b933903ed2b
jlsong <js6855@nyu.edu>
2013-12-11 17:38:57 -0500

    back

---
+++ lib_control.php
@@ -0,0 +1,20 @@
+<?php
+class DBC {
+	public $con;
+	function __construct(){
+		$dbuser = jlsong;
+		$dbpassword = $dbuser;
+		$this->con = pg_connect("host=localhost port=5432 dbname=dbproject user=$dbuser password=$dbpassword");
+		if ($this->con === false) {
+			throw new Exception("cannot connect to database");
+		}
+		$rs = pg_query($this->con, "set search_path to 'dbo';");
+	}
+	
+	function __destruct(){
+		if ($this->con !== false || !isset($this->con) ) {
+			pg_close($this->con);
+		}
+	}
+}
+?>
