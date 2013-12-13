f9533d652e0cec7d57882a8bb3ba8b754a0ba9ba
jlsong <js6855@nyu.edu>
2013-12-13 02:52:26 +0000

    finished

--- delete_pin.php
+++ delete_pin.php
@@ -7,18 +7,28 @@ echo $pin_id."<br>";
 try {
 	$db = new DBC();
 	$con = $db->con;
-	$rs = pg_query($con, "select picture.user_id as owner
+	$rs = pg_query($con, "select 
+		picture.user_id as owner_id,
+		picture.picture_id as picture_id
 		from pin join picture on picture.picture_id = pin.picture_id where pin.pin_id = $pin_id ;");
-	$owner = pg_fetch_all($rs)[0]['owner'];
+	$row = pg_fetch_all($rs)[0];
+	$owner_id = $row['owner_id'];
+	$picture_id = $row['picture_id'];
+
 
 	echo $user_id . "<br>";
-	echo $owner . '<br>';
-	// SHIT
-	exit();
+	echo $ownerid . '<br>';
 
-	$rs = pg_query($con, "delete from pinboard where pinboard_id=$pinboard_id ; ");
-	$rs = pg_fetch_all($rs);
-	header('Location: list_boards.php');
+	if ($user_id != $owner_id) {
+		$rs = pg_query($con, "delete from pin where pin_id=$pin_id; ");
+		pg_fetch_all($rs);
+	} else {
+		$rs = pg_query($con, "delete from pin where pin_id=$pin_id; ");
+		$rs = pg_query($con, "delete from picture where picture_id=$picture_id; ");
+	}
+
+	$backto = $_POST["backto"];
+	header("Location: $backto");
 } catch (Exception $e) {
 	$eurl = "error.php?message=".urlencode($e->getMessage());
 	$eurl .= "&to=".urlencode("register.php");
